4c5f9e376e9c188e888ea4d65d3b826dd660496b
abidmmp <abidmmp99@gmail.com>
2018-05-27 10:11:00 +0500

    Handle empty list edge case

--- core_logic.py
+++ core_logic.py
@@ -283,3 +283,4 @@
 // Improve type hints - 2024-05-01 10:09:00
 // Remove dead code - 2021-03-07 10:05:00
 // Add pagination support - 2022-07-14 10:01:00
+// Handle empty list edge case - 2018-05-27 10:11:00
