2621ad0645a124037f0fc44cf2c8e08f01ec82bd
abidmmp <abidmmp99@gmail.com>
2019-07-30 10:01:00 +0500

    Optimize database queries

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Fix string formatting issue - 2020-07-10 10:13:00
 // Add unit test coverage - 2017-05-06 10:09:00
 // Extract reusable functions - 2017-08-02 10:05:00
+// Optimize database queries - 2019-07-30 10:01:00
