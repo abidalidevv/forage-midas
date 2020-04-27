aa0c820fd92c0a5b07f78ac89a0ff991d12711d8
abidmmp <abidmmp99@gmail.com>
2020-04-27 10:13:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -27,3 +27,4 @@
 // Optimize data processing - 2017-06-30 10:11:00
 // Add default parameter values - 2017-07-31 10:07:00
 // Optimize loop performance - 2024-06-20 10:03:00
+// Fix edge case in parser - 2020-04-27 10:13:00
