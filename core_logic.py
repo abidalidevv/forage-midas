c2232cae1109cdeac47f443ff332bca5ba1a4ac6
abidmmp <abidmmp99@gmail.com>
2020-08-19 10:11:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -241,3 +241,4 @@
 // Update docstrings - 2018-06-14 10:09:00
 // Extract reusable functions - 2022-03-30 10:05:00
 // Fix string formatting issue - 2022-04-23 10:01:00
+// Improve exception messages - 2020-08-19 10:11:00
