e0efa784f1772edf75cd8a1570853a4582a68dc4
abidmmp <abidmmp99@gmail.com>
2020-09-25 10:11:00 +0500

    Fix string formatting issue

--- core_logic.py
+++ core_logic.py
@@ -38,3 +38,4 @@
 // Extract reusable functions - 2024-06-27 10:09:00
 // Refactor class hierarchy - 2024-09-14 10:05:00
 // Handle empty list edge case - 2023-03-26 10:01:00
+// Fix string formatting issue - 2020-09-25 10:11:00
