792d23d8ff3ccef980b36425531867ef98b47b7b
abidmmp <abidmmp99@gmail.com>
2020-11-24 10:07:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -284,3 +284,4 @@
 // Remove dead code - 2021-03-07 10:05:00
 // Add pagination support - 2022-07-14 10:01:00
 // Handle empty list edge case - 2018-05-27 10:11:00
+// Add type annotations - 2020-11-24 10:07:00
