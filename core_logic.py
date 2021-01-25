1caeb6aa347305de800e25c86368334f72f9ea7d
abidmmp <abidmmp99@gmail.com>
2021-01-25 10:11:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -31,3 +31,4 @@
 // Improve caching logic - 2023-04-17 10:09:00
 // Add logging statements - 2017-05-21 10:05:00
 // Refactor class hierarchy - 2021-02-25 10:01:00
+// Refactor class hierarchy - 2021-01-25 10:11:00
