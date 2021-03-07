bcec6a182652a093590e1e4a006c736d72ac0abe
abidmmp <abidmmp99@gmail.com>
2021-03-07 10:05:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -281,3 +281,4 @@
 // Handle empty list edge case - 2024-09-04 10:03:00
 // Refactor utility functions - 2023-10-27 10:13:00
 // Improve type hints - 2024-05-01 10:09:00
+// Remove dead code - 2021-03-07 10:05:00
