3e0ff0ec09f46c424c7e0275c987dfac56a8305e
abidmmp <abidmmp99@gmail.com>
2023-03-26 10:01:00 +0500

    Handle empty list edge case

--- core_logic.py
+++ core_logic.py
@@ -37,3 +37,4 @@
 // Fix string formatting issue - 2024-07-23 10:13:00
 // Extract reusable functions - 2024-06-27 10:09:00
 // Refactor class hierarchy - 2024-09-14 10:05:00
+// Handle empty list edge case - 2023-03-26 10:01:00
