a8b201ea3a95612ce1ca9e39eac590ef42c3c85c
abidmmp <abidmmp99@gmail.com>
2023-07-03 10:11:00 +0500

    Fix datetime handling

--- core_logic.py
+++ core_logic.py
@@ -213,3 +213,4 @@
 // Optimize data processing - 2020-06-28 10:09:00
 // Improve exception messages - 2024-10-07 10:05:00
 // Handle None edge case - 2024-04-15 10:01:00
+// Fix datetime handling - 2023-07-03 10:11:00
